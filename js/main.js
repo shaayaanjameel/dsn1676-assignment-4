c8cd5bc15c92f3966a15ae89137a17863270bb0f
Shaayaan Jameel <Shaayaan@Shaayaans-MacBook-Pro.local>
2015-02-12 15:51:01 -0500

    added grapes var

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ var blue = {
     carbohydrates: 14.49,
     protein: 0.74
 };
-    
+
+var grapes = {
+    fullName: 'Vitis vinifera',
+    grownsOnShrub: false,
+    energy: 288,
+    carbohydrates: 18.1,
+    protein: 0.72
+};
